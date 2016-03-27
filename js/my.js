0e12b6b38095d341ed0b6f97280c88c586c21500
yiwenyi <evenywy@icloud.com>
2016-03-27 13:10:52 +0800

    update commit

--- js/my.js
+++ js/my.js
@@ -1,3 +1,9 @@
 /**
  * Created by Administrator on 2016/3/23.
  */
+$('#myExperience button').click(function(){
+    $('.panel-body').css('display','none');
+    $('#myExperience button').removeClass('btn-info')
+    $(this).addClass('btn-info')
+    $('#Experience'+$(this).attr('id')).css('display','block')
+})
